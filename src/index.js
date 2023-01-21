8a1403d6d633012962843f33af5aedfc4bae491d
RulerOFShadow <43661855+RulerOFShadow@users.noreply.github.com>
2023-01-21 13:40:16 +0800

    last one

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-    </BrowserRouter>
+    <HashRouter >
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    
+    </HashRouter>
   
 );
 
