0200da8f860316e476c4feb08cdd741290a9c48b
RulerOFShadow <43661855+RulerOFShadow@users.noreply.github.com>
2023-01-21 13:26:23 +0800

    trying again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import HomePage from './Pages/Hompage';
 import './App.css';
-import {Link, Route, Router, Routes} from 'react-router-dom';
+import {Route,  Routes} from 'react-router-dom';
 import AnimeDetail from './Pages/AnimeDetailsPage';
 
 function App() {
   return (
-    <Router>
+  
     <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/animeDetail/:id' element={<AnimeDetail/>}/>
     </Routes>
-    </Router>
   );
 }
 
