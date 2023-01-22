a99b23f78d78fdc1a638b849abff6711c065df92
RulerOFShadow <43661855+RulerOFShadow@users.noreply.github.com>
2023-01-22 10:47:56 +0800

    Navbar Responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import '../styles/Nav.css';
 import {Link} from 'react-router-dom';
+import ImageNav from "../img/Animania-noBg.png";
 
 export default function Nav(){
 
  
 
     return(
-        <Link to="/">
+   
         <nav>
-            <img src="https://logos-world.net/wp-content/uploads/2021/08/Akatsuki-Emblem.png" alt="" />
+            <Link to="/">
+            <img src="https://logos-world.net/wp-content/uploads/2021/08/Akatsuki-Emblem.png" className="home" alt="" />
+            
+            <img src={ImageNav} className="animania"/>
+            </Link>
         </nav>
-        </Link>
+        
     )
 }
